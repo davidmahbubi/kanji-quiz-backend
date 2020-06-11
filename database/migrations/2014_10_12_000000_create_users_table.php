64856b49fdb619ef78cefbd8e3ad461cf86c55ff
David Mahbubi <ulrichdavid0370@gmail.com>
2020-06-11 17:18:58 +0700

    Init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->string('username', 64)->unique();
+            $table->string('password', 255);
+            $table->string('picture', 32)->default('default.png');
+            $table->string('api_token')->unique();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
