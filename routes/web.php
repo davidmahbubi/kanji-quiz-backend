6f3c80b933aca195804653155f98bc221fd70920
David Mahbubi <ulrichdavid0370@gmail.com>
2020-06-11 19:26:53 +0700

    Login UI done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard.dashboard');
 });
+
+Route::get('admin/auth/login', function() {
+    return view('auth.login');
+});
