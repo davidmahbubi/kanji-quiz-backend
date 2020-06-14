6a8a4246c6f9a57c2058110e40bf38e225e96b21
David Mahbubi <ulrichdavid0370@gmail.com>
2020-06-14 18:55:48 +0700

    Added level model and migration

---
+++ app/Model/Level/Level.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Level;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Level extends Model
+{
+    //
+}
---
+++ database/seeds/LevelSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Model\Level\Level;
+use Carbon\Carbon;
+
+class LevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Level::truncate();
+        Level::insert([
+            [
+                'name' => 'JLPT N5',
+                'limit' => 10,
+                'score' => 5,
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'JLPT N4',
+                'limit' => 10,
+                'score' => 10,
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'JLPT N3',
+                'limit' => 10,
+                'score' => 15,
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'JLPT N2',
+                'limit' => 10,
+                'score' => 20,
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'JLPT N1',
+                'limit' => 10,
+                'score' => 25,
+                'created_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
